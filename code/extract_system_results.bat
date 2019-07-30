087d7f324312065db92b5bc91d16e73e1404d06c
Geoffroey-Allen Franklin <gfranklin@atsu.edu>
2019-07-30 17:19:08 -0400

    Update to version 2.0
    Adds ability to capture patient information and remarks. Adds better motion detection via Dynamic flag. Corrects License text and removes versioning history from each file.

--- code/extract_system_results.bat
+++ code/extract_system_results.bat
@@ -1,41 +1,14 @@
-@REM	MIT License
-@REM
-@REM	Copyright (c) 2017 Geoffroey-Allen Franklin
-@REM
-@REM	Permission is hereby granted, free of charge, to any person obtaining a copy
-@REM	of this software and associated documentation files (the "Software"), to deal
-@REM	in the Software without restriction, including without limitation the rights
-@REM	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-@REM	copies of the Software, and to permit persons to whom the Software is
-@REM	furnished to do so, subject to the following conditions:
-@REM
-@REM	The above copyright notice and this permission notice shall be included in all
-@REM	copies or substantial portions of the Software.
-@REM
-@REM	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-@REM	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-@REM	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-@REM	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-@REM	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-@REM	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-@REM	SOFTWARE.
+@REM	GNU General Public License v3.0
+@REM  See file "LICENSE" for details.
 @REM
 @REM	Principle Investigator:		Brian Degenhardt, DO
 @REM	Instrumentation:			Zane Starks, MS
 @REM	Code by:					Geoffroey-Allen S. Franklin, MBA, BS, AAS, AdeC, MCP
-@REM	Cite As:					Franklin, G. A.. (2016). 4D Postural Extraction Code [Computer software]. Kirksville, MO: Still Research Institute at A.T. Still University.
-@REM	
-@REM	
+@REM	Cite As:					Geoffroey-Allen Franklin. (2019, July 30). 4D Postural Extraction Code [Computer software]. (Version 2.0.0). Zenodo. https://doi.org/10.5281/zenodo.1407282
+@REM
 @REM	Created: 2016-Jan-14
 @REM	Original Publication:	Degenhardt, B., Starks, Z., Bhatia, S., & Franklin, G. A. (2017). Appraisal of the DIERS method for calculating postural measurements: an observational study. Scoliosis and Spinal Disorders, 12(1), 28.
-@REM
-@REM	
-@REM	Change Log:	2016-Jan-29 - Version 1.0 release. Initial release.
-@REM				2016-Feb-09 - Version 1.1 release. Corrected bug #ZS-001: Remove hard count for average->file write to float number based on Day change.
-@REM				2016-Feb-10 - Version 1.2 release. Added motion flag.
-@REM				2016-Jul-13 - Version 1.3 release. Added variable PlumbResult_8001_WeissLAPos_T12.
-@REM				2017-Nov-09 - Moved to Github repo.
-@REM				---
+@REM	---
 
 @ECHO OFF
 setlocal enabledelayedexpansion
@@ -43,12 +16,17 @@ echo Start of command: !time!
 
 ECHO Delete old data: !time!.
 del /F /Q res_filelist.txt
+del /F /Q remarks_filelist.txt
 
 ECHO Building file list: !time!.
-@REM This is for local data source...
-dir /A-D /B /OD /S "..\raw_data\Cleaned Study Data\Average.avr" >> res_filelist.txt
+dir /A-D /B /OD /S "PATH:\TO\THE\DATA\FILE\STORE\*age.avr" | findstr /v "\Repository" | findstr /v "Pedoscan" >> res_filelist.txt
+dir /A-D /B /OD /S "PATH:\TO\THE\DATA\FILE\STORE\Remarks.ini" | findstr /v "\Repository" >> remarks_filelist.txt
 
 ECHO Extracting Data: !time!.
 php extract_system_results.php
+
+ECHO Cleanup: !time!.
+del /F /Q res_filelist.txt
+del /F /Q remarks_filelist.txt
 
 echo End of commands: !time!
--- code/logger.php
+++ code/logger.php
@@ -1,43 +1,15 @@
 <?php
-/* 
-	MIT License
-
-	Copyright (c) 2017 Geoffroey-Allen Franklin
-
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
-
-	The above copyright notice and this permission notice shall be included in all
-	copies or substantial portions of the Software.
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-	SOFTWARE.
+/*
+	GNU General Public License v3.0
+	See file "LICENSE" for details.
 
 	Principle Investigator:		Brian Degenhardt, DO
 	Instrumentation:			Zane Starks, MS
 	Code by:					Geoffroey-Allen S. Franklin, MBA, BS, AAS, AdeC, MCP
-	Cite As:					Franklin, G. A.. (2016). 4D Postural Extraction Code [Computer software]. Kirksville, MO: Still Research Institute at A.T. Still University.
-	
-	
+	Cite As:					Geoffroey-Allen Franklin. (2019, July 30). 4D Postural Extraction Code [Computer software]. (Version 2..0.0). Zenodo. https://doi.org/10.5281/zenodo.1407282
+
 	Created: 2016-Jan-14
 	Original Publication:	Degenhardt, B., Starks, Z., Bhatia, S., & Franklin, G. A. (2017). Appraisal of the DIERS method for calculating postural measurements: an observational study. Scoliosis and Spinal Disorders, 12(1), 28.
-
-	
-	Change Log:	2016-Jan-29 - Version 1.0 release. Initial release.
-				2016-Feb-09 - Version 1.1 release. Corrected bug #ZS-001: Remove hard count for average->file write to float number based on Day change.
-				2016-Feb-10 - Version 1.2 release. Added motion flag.
-				2016-Jul-13 - Version 1.3 release. Added variable PlumbResult_8001_WeissLAPos_T12.
-				2017-Nov-09 - Moved to Github repo.
-				---
 */
 
 //General Rules
@@ -47,24 +19,28 @@ $rule_3 = "Disallow:harm to self";
 if (($rule_1 != TRUE) || ($rule_2 != TRUE) || ($rule_3 != TRUE)) {echo "Protect! Obey! Survive!\n"; die;}
 date_default_timezone_set('America/Chicago'); //hard set for Kirksville.
 
-//Logger write to logfile.txt
+//Logger write to logfile
 	function logger($logline, $logmsg)
 		{
-			$logger_csv = fopen("..\output_data\logfile.txt","a");
+			global $logger_fileext;
+			if (!isset($logger_fileext)) { $logger_fileext = "unknown"; }
+			$logger_csv = fopen("..\output_data\\4D_Deirs_" . $logger_fileext . ".logfile." . date('Y.m.d') .".txt","a");
 			fwrite($logger_csv, "" . $_SERVER['PHP_SELF'] . "|" . date('M d H:i:s') . "|" . $logline. "|". $logmsg . "\r\n");
 			fclose($logger_csv);
 		}
-	
+
+//logger writes to errorfile
 	function recordError($errno,$errstr,$error_file,$error_line)
 		{
-			$logger_csv = fopen("logfile.txt","a");
+			global $logger_fileext;
+			if (!isset($logger_fileext)) { $logger_fileext = "unknown"; }
+			$logger_csv = fopen("..\output_data\\4D_Deirs_" . $logger_fileext . ".errorfile." . date('Y.m.d') .".txt","a");
 			fwrite($logger_csv, "\r\nError --> [ " . $errno . " ] " . $errstr . "  on line: " . $error_line . "\r\n -- File: " . $error_file . "\r\n\r\n");
 			fclose($logger_csv);
 		}
-		
+
 	set_error_handler("recordError");
 	$start_memory = memory_get_usage();
 	logger(__LINE__, "===== Start of Log. =====");
 	logger(__LINE__, "My timezone is: " . date_default_timezone_get());
-
 ?>
